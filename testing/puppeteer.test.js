48f5c1481ca00ebcfa27ad225df063daee09c3bd
Andrewphanguyen <97638780+Andrewphanguyen@users.noreply.github.com>
2024-06-04 12:09:03 -0700

    added tests for tasklist

--- testing/puppeteer.test.js
+++ testing/puppeteer.test.js
@@ -42,6 +42,74 @@ describe('Basic user path in homepage', () => {
     expect(taskCount).toBe(1); // Modify the expected value based on initial number of tasks
   });
 
+  it('Add a task, write the task, and choose a color', async () => {
+    console.log('Testing task addition, title setting, and color selection...');
+
+    await page.setViewport({ width: 1200, height: 800 });
+    await page.click('.add-task-btn');
+  
+    const taskInputSelector = '.task-list .task:last-child .task-input';
+    await page.waitForSelector(taskInputSelector);
+    const taskInput = await page.$(taskInputSelector);
+
+    const taskTitle = 'New Task Title for Testing';
+    await taskInput.type(taskTitle);
+  
+    const colorButton = '.task-list .task:last-child .color-button';
+    const colorButtonSelector = '.task-list .task:last-child .color-button';
+    await page.hover(colorButton);
+    await page.click(colorButtonSelector);
+  
+    const enteredTitle = await page.evaluate(selector => {
+      return document.querySelector(selector).textContent;
+    }, taskInputSelector);
+  
+    const backgroundColor = await page.evaluate(selector => {
+      const task = document.querySelector(selector);
+      return window.getComputedStyle(task).backgroundColor;
+    }, '.task-list .task:last-child');
+  
+    expect(enteredTitle).toBe(taskTitle);
+    expect(backgroundColor).toBe('rgb(195, 128, 204)'); 
+  });
+
+  it('Edit the task and delete it', async() => {
+    const taskInputSelector = '.task-list .task:last-child .task-input';
+    await page.waitForSelector(taskInputSelector);
+    const taskInput = await page.$(taskInputSelector);
+
+    for (let i = 0; i < 27; i++) {
+        await taskInput.press('Backspace');
+    }
+      
+    const taskTitle = 'Editing Task Title';
+    await taskInput.type(taskTitle);
+  
+    const colorButton = '.task-list .task:last-child .color-buttons';
+    await page.hover(colorButton);
+
+    await page.evaluate(() => {
+      const colorButtonSelector = '.task-list .task:last-child .color-buttons #blue';
+      document.querySelector(colorButtonSelector).click();
+    });    
+  
+    const enteredTitle = await page.evaluate(selector => {
+      return document.querySelector(selector).textContent;
+    }, taskInputSelector);
+  
+    const backgroundColor = await page.evaluate(selector => {
+      const task = document.querySelector(selector);
+      return window.getComputedStyle(task).backgroundColor;
+    }, '.task-list .task:last-child');
+  
+    expect(enteredTitle).toBe(taskTitle);
+    expect(backgroundColor).toBe('rgb(107, 177, 217)'); 
+  });
+
+
+
+
+
 
 
   it('Resize window', async () => {
@@ -55,26 +123,21 @@ describe('Basic user path in homepage', () => {
     expect(fullCalendar.includes('active')).toBe(false);
   });
 
-  it('Click next month', async () => {
+  it('Testing next month', async () => {
     console.log('Testing to change months');
-    
-    const name = "June";
-    const monthHeader = await page.$('#month');
+    let initialMonth = await page.evaluate(() => document.querySelector('#month').textContent);
+    console.log(`Initial month: ${initialMonth}`);
+    await page.click('#next-date-btn');
+    // get the new month name
+    let newMonth = await page.evaluate(() => document.querySelector('#month').textContent);
+    console.log(`New month after click: ${newMonth}`);
+    let expectedNewMonth = "July";
+    expect(newMonth).toBe(expectedNewMonth);
+});
 
 
 
-    await page.evaluate(() => {
-      document.querySelector('#next-date-btn').click();
-    });    
-    
-
-    const textCont = await page.evaluate(() => {
-      return monthHeader => monthHeader.textContent;
-    });
-    expect(textCont).toBe(name);
-
-
-  });
+ 
 
 
 
